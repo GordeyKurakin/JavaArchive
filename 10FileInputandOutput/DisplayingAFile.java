4187799c792aef6d628e35d29efb148dc36b3907
GordeyKurakin <gordeykurakin@gmail.com>
2017-09-24 17:49:38 +0200

    I have don 10.

--- 10FileInputandOutput/DisplayingAFile.java
+++ 10FileInputandOutput/DisplayingAFile.java
@@ -1,25 +1,21 @@
-import static java.nio.file.StandardOpenOption.*;
-import java.nio.file.*;
-import java.io.*;
+import java.io.File;
 import java.util.Scanner;
 
 public class DisplayingAFile {
 
-  public static void main(String[] args) {
+  public static void main(String[] args)  throws Exception{
     Scanner keyboard = new Scanner(System.in);
-    String fil;
+    String fil, line;
     System.out.print("Open which file: ");
     fil = keyboard.nextLine();
-    Path file = Paths.get("./" + fil);;
-    try (InputStream in = Files.newInputStream(file);
-        BufferedReader reader =
-          new BufferedReader(new InputStreamReader(in))) {
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-    } catch (IOException x) {
-        System.err.println(x);
+    File txtfile = new File(fil);
+    keyboard.close();
+    Scanner readfile = new Scanner(txtfile);
+    
+    while (readfile.hasNext() )
+    {
+        line = readfile.nextLine();
+        System.out.println( line );
     }
   }
 }
